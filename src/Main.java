49e05cf0612b5062aaafb92f1a0ab4c5dded5052
Yevhen Prokopenko <yevhen@MacBook-Yevhen.local>
2016-06-28 09:16:04 +0300

    first enum commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    private UserType u;
+    private void someTempFunc(){
+        u = UserType.ADMIN;
+        String b = "ADMIN";
+
+    }
+
+}
---
+++ src/UserType.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yevhen on 27.06.16.
+ */
+public enum UserType {
+    ADMIN,
+    BUYER,
+    SELLER;
+}
